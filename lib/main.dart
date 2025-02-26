729cbba94fa4241016a7d90ff937350afded2907
rchai12 <digital24688@live.com>
2025-02-26 18:23:04 -0500

    added them mode, can change text colors

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
+
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool _isDarkMode = false;
+
+  void toggleTheme(bool value) {
+    setState(() {
+      _isDarkMode = value;
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: FadingTextAnimation(),
+      theme: _isDarkMode ? ThemeData.dark() : ThemeData.light(),
+      home: FadingTextAnimation(onThemeToggle: toggleTheme, isDarkMode: _isDarkMode),
     );
   }
 }
 
 class FadingTextAnimation extends StatefulWidget {
+  final Function(bool) onThemeToggle;
+  final bool isDarkMode;
+
+  const FadingTextAnimation({Key? key, required this.onThemeToggle, required this.isDarkMode}) : super(key: key);
+
   @override
   _FadingTextAnimationState createState() => _FadingTextAnimationState();
 }
 
 class _FadingTextAnimationState extends State<FadingTextAnimation> {
   bool _isVisible = true;
+  Color _textColor = Colors.black;
 
   void toggleVisibility() {
     setState(() {
       _isVisible = !_isVisible;
+    });
+  }
+
+  void changeTextColor(Color color) {
+    setState(() {
+      _textColor = color;
     });
   }
 
@@ -32,6 +61,12 @@ class _FadingTextAnimationState extends State<FadingTextAnimation> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Fading Text Animation'),
+        actions: [
+          Switch(
+            value: widget.isDarkMode,
+            onChanged: widget.onThemeToggle,
+          ),
+        ],
       ),
       body: Center(
         child: AnimatedOpacity(
@@ -39,13 +74,43 @@ class _FadingTextAnimationState extends State<FadingTextAnimation> {
           duration: Duration(seconds: 1),
           child: Text(
             'Hello, Flutter!',
-            style: TextStyle(fontSize: 24),
+            style: TextStyle(fontSize: 24, color: _textColor),
           ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: toggleVisibility,
-        child: Icon(Icons.play_arrow),
+      floatingActionButton: Stack(
+        children: [
+          Align(
+            alignment: Alignment.bottomRight,
+            child: FloatingActionButton(
+              onPressed: toggleVisibility,
+              child: Icon(Icons.play_arrow),
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: FloatingActionButton(
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (context) => AlertDialog(
+                    title: Text('Pick Text Color'),
+                    content: SingleChildScrollView(
+                      child: BlockPicker(
+                        pickerColor: _textColor,
+                        onColorChanged: (color) {
+                          changeTextColor(color);
+                          Navigator.of(context).pop();
+                        },
+                      ),
+                    ),
+                  ),
+                );
+              },
+              child: Icon(Icons.palette),
+            ),
+          ),
+        ],
       ),
     );
   }
